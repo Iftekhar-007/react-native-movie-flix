270cfda9cd7236ee667670c2afb3ce7f3e442092
Iftekhar-007 <shawonsp534@gmail.com>
2025-10-18 19:00:55 +0600

    okay

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,45 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Image, ScrollView, View } from "react-native";
 
 export default function Index() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const API_KEY = process.env.EXPO_TMDB_HEADER;
+  // const fetchData = fetch(
+  //   "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+  // );
+
+  const API_URL = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${API_KEY}`;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [API_URL]);
+
+  if (loading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color="#00BFFF"
+        style={{ marginTop: 50 }}
+      />
+    );
+  }
+
   const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODc5YTE3NDlmZThiZDNjNGI0NTg4ZWZjZmYzNjlhZiIsIm5iZiI6MTc2MDcwNjE1NS40NzcsInN1YiI6IjY4ZjIzZTZiOWE5OGVlNWQ0MjU5MTE3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wKwYuQ_VE6P9IAMpIeSG0X5g6NP21rJOCF8_ONNW86M'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
